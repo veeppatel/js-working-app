74d13d794b331dc52423d75340d0d0ca9b8187d3
veeppatel <31413195+veeppatel@users.noreply.github.com>
2025-01-15 14:00:49 +0000

    Learned the variables

---
+++ 01_basics/variable.js
@@ -0,0 +1,17 @@
+const accountName = "veep"
+let accountEmail = "veep@gmail.com"
+var accountCity = "Jaipur"
+accountState = "rajasthan"
+
+console.log(accountState)
+
+ //accountName = "veep1"
+ accountEmail = "veep1@gmail.com"
+
+ //Not to use var due to issues in block scope 
+var accountCity = "Jai1pur"
+accountState = "rajasthan1"
+
+console.table([accountName, accountEmail, accountCity, accountState])
+
+//
